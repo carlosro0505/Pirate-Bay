f98207420048472fae4875a7a199d6e6dc72d9f0
carlosro0505 <carlosro0505@gmail.com>
2023-11-22 14:36:44 -0800

    added search and bar buttons to categories.java

--- src/Categories.java
+++ src/Categories.java
@@ -1,12 +1,16 @@
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Label;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
-public class Categories extends Application {
+public class Categories extends Application{
     private Stage primaryStage;
 
     public Categories(Stage primaryStage) {
@@ -14,13 +18,22 @@ public class Categories extends Application {
     }
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws FileNotFoundException{
         Screen screen = Screen.getPrimary();
         Rectangle2D bounds = screen.getVisualBounds();
 
-        Label label = new Label("Carlos Rodriguez");
-        AnchorPane root = new AnchorPane(label);
+        VBox searchAndBtns = new SearchBarAndButtonsHelper().createBar();
+        // searchAndBtns.setSpacing(5.0);
+        searchAndBtns.setAlignment(Pos.CENTER);
+
+        AnchorPane.setTopAnchor(searchAndBtns, 0.025 * bounds.getHeight());
+        AnchorPane.setLeftAnchor(searchAndBtns, 0.0);
+        AnchorPane.setRightAnchor(searchAndBtns, 0.0);
+
+        AnchorPane root = new AnchorPane(searchAndBtns);
+        root.getStyleClass().add("search-background");
         Scene scene = new Scene(root, bounds.getWidth(), bounds.getHeight());
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
