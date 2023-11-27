291132923ed0bbebed0c80beb099eeabe629d87e
aaselvam13 <62858943+aaselvam13@users.noreply.github.com>
2023-11-27 00:19:36 -0800

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -64,7 +64,7 @@ public class Item extends Application {
         downDesc.setStyle("-fx-font: 16 verdana; -fx-text-fill: #a4a4ff; -fx-font-weight: bold");
 
         //rectangle containing the uppermost header
-        Rectangle titleRect = new Rectangle(bounds.getWidth(), 0.05*bounds.getHeight());
+        Rectangle titleRect = new Rectangle(1540.0, 0.05*785);
         titleRect.setFill(Color.web("#181c24"));
         //stackpane putting header text on rectangle;
         StackPane titleFrame = new StackPane();
@@ -72,12 +72,12 @@ public class Item extends Application {
         StackPane.setAlignment(title,Pos.CENTER);
 
         //rectangle containing the header for file details
-        Rectangle infoRect = new Rectangle(0.3*bounds.getWidth(), 0.05*bounds.getHeight());
+        Rectangle infoRect = new Rectangle(0.3*1540.0, 0.05*785);
         infoRect.setFill(Color.web("#181c24"));
         //TextArea containing file details
         TextArea details = new TextArea(finalDetails);
-        details.setMinHeight(0.45*bounds.getHeight());
-        details.setMaxWidth(0.3*bounds.getWidth());
+        details.setMinHeight(0.45*785);
+        details.setMaxWidth(0.3*1540.0);
         details.setStyle("-fx-control-inner-background: #2d3545; -fx-font: 16 verdana; -fx-text-fill: #c7c7ff; -fx-text-box-border: transparent; -fx-focus-color: transparent;");
         details.setEditable(false);
         //details.setAlignment(Pos.TOP_LEFT);
@@ -90,12 +90,12 @@ public class Item extends Application {
         infoFrame.getChildren().addAll(infoBar, details);
 
         //rectangle containing the header for file description
-        Rectangle textRect = new Rectangle(0.3*bounds.getWidth(), 0.05*bounds.getHeight());
+        Rectangle textRect = new Rectangle(0.3*1540.0, 0.05*785);
         textRect.setFill(Color.web("#181c24"));
         //TextArea containing the file description
         TextArea descript = new TextArea(description);
-        descript.setMaxHeight(0.2*bounds.getHeight());
-        descript.setMaxWidth(0.3*bounds.getWidth());
+        descript.setMaxHeight(0.2*785);
+        descript.setMaxWidth(0.3*1540.0);
         descript.setStyle("-fx-control-inner-background: #2d3545; -fx-font: 14 verdana; -fx-text-fill: #c7c7ff; -fx-text-box-border: transparent; -fx-focus-color: transparent;");
         descript.setEditable(false);
         //descript.setAlignment(Pos.TOP_LEFT);
@@ -107,12 +107,12 @@ public class Item extends Application {
         VBox textFrame = new VBox(0);
         textFrame.getChildren().addAll(textBar, descript);
 
-        Rectangle buttonRect1 = new Rectangle(0.3*bounds.getWidth(),0.05*bounds.getHeight());
+        Rectangle buttonRect1 = new Rectangle(0.3*1540.0,0.05*785);
         StackPane buttonRect1Pane = new StackPane();
         buttonRect1Pane.getChildren().addAll(buttonRect1, downDesc);
         StackPane.setAlignment(downDesc, Pos.CENTER);
 
-        Rectangle buttonRect2 = new Rectangle(0.3*bounds.getWidth(), 0.15*bounds.getHeight());
+        Rectangle buttonRect2 = new Rectangle(0.3*1540.0, 0.15*785);
         buttonRect1.setFill(Color.web("#181c24"));
         buttonRect2.setFill(Color.web("#2d3545"));
         buttonRect2.setArcWidth(5);
@@ -157,25 +157,25 @@ public class Item extends Application {
         buttonFrame.getChildren().addAll(buttonRect1Pane,buttonPane);
 
 
-        AnchorPane.setTopAnchor(searchAndBtns, 0.025 * bounds.getHeight());
+        AnchorPane.setTopAnchor(searchAndBtns, 0.025 * 785);
         AnchorPane.setLeftAnchor(searchAndBtns, 0.0);
         AnchorPane.setRightAnchor(searchAndBtns, 0.0);
 
-        AnchorPane.setTopAnchor(titleFrame, 0.3*bounds.getHeight());
+        AnchorPane.setTopAnchor(titleFrame, 0.3*785);
 
-        AnchorPane.setTopAnchor(infoFrame, 0.4*bounds.getHeight());
-        AnchorPane.setLeftAnchor(infoFrame, 0.15*bounds.getWidth());
+        AnchorPane.setTopAnchor(infoFrame, 0.4*785);
+        AnchorPane.setLeftAnchor(infoFrame, 0.15*1540.0);
 
-        AnchorPane.setTopAnchor(textFrame, 0.4*bounds.getHeight());
-        AnchorPane.setLeftAnchor(textFrame, 0.5*bounds.getWidth());
+        AnchorPane.setTopAnchor(textFrame, 0.4*785);
+        AnchorPane.setLeftAnchor(textFrame, 0.5*1540.0);
 
-        AnchorPane.setTopAnchor(buttonFrame, 0.7*bounds.getHeight());
-        AnchorPane.setLeftAnchor(buttonFrame,0.5*bounds.getWidth());
+        AnchorPane.setTopAnchor(buttonFrame, 0.7*785);
+        AnchorPane.setLeftAnchor(buttonFrame,0.5*1540.0);
 
         AnchorPane root = new AnchorPane();
         root.getChildren().addAll(searchAndBtns, titleFrame,infoFrame, textFrame, buttonFrame);
         root.getStyleClass().add("search-page-contrast");
-        Scene scene = new Scene(root, bounds.getWidth(), bounds.getHeight());
+        Scene scene = new Scene(root, 1540.0, 785);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
